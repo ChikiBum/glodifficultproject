abbbfd9132b546e62a6c2c403d7dc2f58d713ec2
Alexander Bozhko <obozhko82@gmail.com>
2020-08-12 00:30:11 +0300

    Hard lesson 4

--- scripts/script.js
+++ scripts/script.js
@@ -1,53 +1,20 @@
 'use strict';
 
-let lang,
-    arrDays = ['ru', ['Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье'], 
-    'en', ['Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday']],
-    indexLang,
-    namePerson;
-
-    lang = prompt('Choose language RU or EN', 'ru').toLowerCase();
-    namePerson = prompt('Enter Name', 'Артем').toLowerCase();
-
-    console.group('------------- Переменная lang может принимать 2 значения: ru en. через if-----------------');
-    if (lang === 'ru'){
-        console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
-    } 
-    else if (lang === 'en'){
-        console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
-    }
-    else {
-        console.log('Wrong language. Choose language RU or EN');
-    }
-    console.groupEnd();    
-    
-
-    console.group('------------- через switch-case -----------------');
-    
-    switch(lang){
-        case 'ru': console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
-            break;
-        case 'en': console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
-            break;
-        default: console.log('Wrong language. Choose language RU or EN');
+let charLine = prompt('Введите строчку', 'это строка');
+  
+let inputLine = function(line){
+    let trimLine;
+    if (line !== null){
+       trimLine = line.trim(); 
     }
     
-    console.groupEnd();
+    if(typeof(line) !== 'string' || line === null ){
+        return(typeof(line) + ' - это HE строка!');
+    } else if (trimLine.length > 30){
+        return(trimLine.slice(0, 31) + '...');
+    } else {
+    return(trimLine);
+    }
+};
 
-
-    console.group('------------- через многомерный массив без ифов и switch. -----------------');
-    
-    indexLang = arrDays.indexOf(lang);
-    console.log(arrDays[indexLang + 1][0]);
-    
-    console.groupEnd();
-
-
-    console.group('------------- У нас есть переменная namePerson -----------------');
-    
-    namePerson === 'артем' ? console.log('директор') : 
-    namePerson === 'максим' ? console.log('преподаватель') :  console.log('студент');
-
-    console.groupEnd();
-
-
+console.log(inputLine(charLine));
