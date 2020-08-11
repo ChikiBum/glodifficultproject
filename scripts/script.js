4d588e1780ce96ad3f7cf0dc9129b65e600d2f99
Alexander Bozhko <obozhko82@gmail.com>
2020-08-11 19:37:24 +0300

    Hard lesson 3

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,53 @@
-let num  = 266219,
-    numToString,
-    multiply = 1 ; 
-        
-    console.group('-------------Вывести в консоль произведение (умножение) цифр этого числа-----------------');
+'use strict';
 
-    numToString = num + '';
+let lang,
+    arrDays = ['ru', ['Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье'], 
+    'en', ['Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday']],
+    indexLang,
+    namePerson;
 
-    for (let i = 0; i < numToString.length; i++)
-    {
-        multiply = multiply * numToString[i];
-        
+    lang = prompt('Choose language RU or EN', 'ru').toLowerCase();
+    namePerson = prompt('Enter Name', 'Артем').toLowerCase();
+
+    console.group('------------- Переменная lang может принимать 2 значения: ru en. через if-----------------');
+    if (lang === 'ru'){
+        console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+    } 
+    else if (lang === 'en'){
+        console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
     }
-
-    console.log( `Multiply numbers in a number ${num} is ${multiply}`);
-    console.groupEnd();
-    
-    console.group('------------- Полученный результат возвести в степень 3 не Math.pow)-----------------');
-    console.log( `А number ${multiply} in power 3 is ${multiply**3}`);
-    console.groupEnd();
-
-    console.group('------------- Полученный результат возвести в степень 3 не Math.pow)-----------------');
-    console.log( `Fist two numbers of ${num} are ${numToString.slice(0,2)}` );
-        console.groupEnd();    
+    else {
+        console.log('Wrong language. Choose language RU or EN');
+    }
+    console.groupEnd();    
     
 
+    console.group('------------- через switch-case -----------------');
+    
+    switch(lang){
+        case 'ru': console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+            break;
+        case 'en': console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+            break;
+        default: console.log('Wrong language. Choose language RU or EN');
+    }
+    
+    console.groupEnd();
+
+
+    console.group('------------- через многомерный массив без ифов и switch. -----------------');
+    
+    indexLang = arrDays.indexOf(lang);
+    console.log(arrDays[indexLang + 1][0]);
+    
+    console.groupEnd();
+
+
+    console.group('------------- У нас есть переменная namePerson -----------------');
+    
+    namePerson === 'артем' ? console.log('директор') : 
+    namePerson === 'максим' ? console.log('преподаватель') :  console.log('студент');
+
+    console.groupEnd();
 
 
