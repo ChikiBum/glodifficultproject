47251a4763f35e4058e660f0d429e6643bb8f0a4
Alexander Bozhko <obozhko82@gmail.com>
2020-08-12 23:30:39 +0300

    Hard lesson05 init

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,41 @@
 'use strict';
 
-let charLine = prompt('Введите строчку', 'это строка');
-  
-let inputLine = function(line){
-    let trimLine;
-    if (line !== null){
-       trimLine = line.trim(); 
-    }
-    
-    if(typeof(line) !== 'string' || line === null ){
-        return(typeof(line) + ' - это HE строка!');
-    } else if (trimLine.length > 30){
-        return(trimLine.slice(0, 31) + '...');
-    } else {
-    return(trimLine);
-    }
-};
+/*1) Создать массив arr = []
+— Записать в него 7 любых многозначных чисел в виде строк
+— Вывести в консоль только те, что начинаются с цифры 2 или 4 (Должны присутствовать в массиве)*/
 
-console.log(inputLine(charLine));
+let arr  = [];
+
+arr.push('2345', '345345', '43445', '634234', '2278678', '1231');
+
+const arrResMap = arr.map(function(num){
+ 
+ if (num[0] === '2' || num[0] === '4'){
+ return num;
+ }
+}); 
+
+const arrResFilter = arrResMap.filter(function(x) {
+    return x !== undefined; 
+});
+
+console.log(arr);
+console.log(arrResMap);
+console.log(arrResFilter);
+
+/*2) Вывести в столбик все простые числа от 1 до 100
+    — Статья про простые числа 
+    — Рядом с каждым числом написать оба делителя данного числа
+    Например: “Делители этого числа: 1 и n”*/
+
+
+goLable:
+for (let i = 2; i <= 100; i++) { 
+
+  for (let j = 2; j < i; j++) { 
+    if (i % j === 0) 
+    {continue goLable;} 
+  }
+
+  console.log('Простое число: ', i, 'Делители этого числа: 1 и ', i );  
+}
