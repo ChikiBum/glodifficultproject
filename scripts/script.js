e049d0d4cfd9ae4767113ec4ab6b8ed5a9b354d8
Alexander Bozhko <obozhko82@gmail.com>
2020-08-13 16:43:45 +0300

    Change simple numbers defenition method

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,7 @@
 — Вывести в консоль только те, что начинаются с цифры 2 или 4 (Должны присутствовать в массиве)*/
 
 let arr  = [];
+let arr2  = [];
 
 arr.push('2345', '345345', '43445', '634234', '2278678', '1231');
 
@@ -29,13 +30,45 @@ console.log(arrResFilter);
     Например: “Делители этого числа: 1 и n”*/
 
 
-goLable:
-for (let i = 2; i <= 100; i++) { 
+// goLable:
+// for (let i = 2; i <= 100; i++) { 
 
-  for (let j = 2; j < i; j++) { 
-    if (i % j === 0) 
-    {continue goLable;} 
+//   for (let j = 2; j < i; j++) { 
+//     if (i % j === 0) 
+//     {continue goLable;} 
+//   }
+
+//   console.log('Простое число: ', i, 'Делители этого числа: 1 и ', i );  
+// }
+
+let startNumber = +prompt('Eneter startNumber', 2),
+    endNumber = +prompt('Eneter endNumber', 10);
+
+const naturalNumber = function(startNumber, endNumber, callback){
+  if (startNumber > 1 && startNumber < endNumber) {
+    for (let i = startNumber; i <= endNumber; i++) { 
+      callback(i);
+    }
+    }
+   else {
+      return 'startNumber may by more that 1 and startNumber > endNumber';
   }
+};
 
-  console.log('Простое число: ', i, 'Делители этого числа: 1 и ', i );  
+const naturalNumberInside = function(n){
+  for (let j = startNumber; j <= n; j++){
+    if (j === n){
+      arr2.push(`Простое число: ${n} Делители этого числа: 1 и ${n}`);
+    } else if (n % j === 0)
+    {return;}
+  }
+};
+
+const result = naturalNumber(startNumber, endNumber, naturalNumberInside);
+    if (result === undefined) {
+      for (let i = 0; i < arr2.length; i++){
+        console.log(arr2[i]);
+      }
+    } else {
+console.log(result);
 }
