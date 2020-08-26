11134d4e838d1cbe6902de685ed010dd9d9c8180
Alexander Bozhko <obozhko82@gmail.com>
2020-08-26 22:20:48 +0300

    Les14 hard init

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,58 @@
-'use strict';
+"use strict";
 
-const changeBtn = document.getElementById('change'),
-    h1Color = document.getElementById('color'),
-    wrapper = document.getElementsByClassName('wrapper') ;
+const wrapper = document.querySelector('.wrapper');
 
- 
-    const getRandomNuber = function (){
-        return Math.floor(Math.random() * (256));
-    };
 
-    const changeColor = function(e){
-        const r = getRandomNuber().toString(16),
-            g = getRandomNuber().toString(16),
-            b = getRandomNuber().toString(16);
-         
-        h1Color.textContent = '#' + r + g + b;
-        wrapper[0].setAttribute('style', `background-color: #${r}${g}${b};`);
+function DomElement(selector, height, width, bg, fontSize){
+  this.selector = selector;
+  this.height = height;
+  this.width = width;
+  this.bg = bg;
+  this.fontSize = fontSize;
+  this.position = 'absolute';
+  this.top = 0;
+  this.left = 0;
+  }
+  
+  DomElement.prototype.createElement = function(){
+     if (this.selector[0] === '.'){
+           if (document.querySelector('.block')){
+            const el = document.querySelector('.block');
+            el.parentNode.removeChild(el);
+           }
+      const arr = this.selector.split(''),
+                 className = arr.splice(1, arr.length);
+      const div = document.createElement('div');
+            div.classList.add(className.join(''));
+            div.textContent = 'Its DIV element';
+            wrapper.append(div);
+            div.setAttribute('style', `height:${this.height}px; width:${this.width}px; 
+            background-color:${this.bg}; font-size:${this.fontSize}em; position: ${this.position};
+            top: ${this.top}px; left: ${this.left}px`);
+      } 
+  };
 
-    };
+const newElement1 = new DomElement('.block', 100, 100, '#FF9', 1);
 
-    changeBtn.addEventListener('click', changeColor);
+newElement1.createElement();
+
+
+DomElement.prototype.moveElement = function(event) {
+      const block = document.querySelector('.block');
+      if (event.code === 'ArrowRight'){
+            screen.width - this.width > this.left ? this.left += 5 : alert('End of screen');
+            newElement1.createElement();
+      } else if (event.code === 'ArrowLeft'){
+            this.left > 0  ? this.left -= 5 : alert('End of screen');
+            newElement1.createElement();
+      } else if (event.code === 'ArrowUp'){
+            this.top > 0  ? this.top -= 5 : alert('End of screen');
+            newElement1.createElement();
+      } else if (event.code === 'ArrowDown'){
+            screen.height - this.height > this.top ? this.top += 5 : alert('End of screen');
+            newElement1.createElement();
+      }
+  };
+
+document.addEventListener('keydown', newElement1.moveElement.bind(newElement1));
+
